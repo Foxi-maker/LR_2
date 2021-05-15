558166188c9f808bb4871b99ef1e88bf2c524243
Foxi Noxi <trolinglisa@gmail.com>
2021-05-16 01:07:43 +0300

    0.1

--- LR_2/Equation.cpp
+++ LR_2/Equation.cpp
@@ -1,10 +1,10 @@
 #include "Equation.h"
 
-Equation::Equation(std::function<double(double, double)> TCX,std::string na)
+Equation::Equation(std::function<double(double, double)> TCX, std::string na)
 {
 	K = TCX;
 
-	out.open("OUTPUT\\"+na+".txt");
+	out.open("OUTPUT\\" + na + ".txt");
 	if (!out.is_open())
 	{
 		std::cout << "Error! File for writing was not open!";
@@ -16,19 +16,18 @@ Equation::Equation(std::function<double(double, double)> TCX,std::string na)
 void Equation::Scheme(double sig)
 {
 	//Шаг по времени и по положению
-	//double tau = 0.01, h = 0.1;
-	double tau = 0.0025, h = 0.1/2;
+	double tau = 0.01, h = 0.1;
+	//double tau = 0.0025, h = 0.1 / 2;
 
 	int Nx = L / h + 1;
 	int Nt = T / tau + 1;
 
 	//Сетка по положению и времени
-	std::vector<std::vector<double>> grid(Nt, std::vector<double>(Nx));
+	std::vector<std::vector<double>> grid(2, std::vector<double>(Nx));
 
-	//Заполение значениями начальных и краевых условий
+	//Заполение значениями начальных условий
 	double u0 = uRight;
 	double x = 0;
-
 	for (int pIter = 0; pIter < Nx; pIter++)
 	{
 		grid[0][pIter] = sin(PI*x);
@@ -37,41 +36,49 @@ void Equation::Scheme(double sig)
 	}
 
 	//Занчение t_0 по варианту
-	double t0 = .5;
-	double time = tau;
-	for (int tIter = 1; tIter < Nt; tIter++)
-	{
-		//if (time < t0)
-		//	grid[tIter][0] = uLeft;
-		//else
-		//	grid[tIter][0] = 0;
+	//double t0 = .5;
+	//double time = tau;
+	//for (int tIter = 1; tIter < Nt; tIter++)
+	//{
+	//	//if (time < t0)
+	//	//	grid[tIter][0] = uLeft;
+	//	//else
+	//	//	grid[tIter][0] = 0;
 
-		//grid[tIter].back() = u0;
-		grid[tIter].front() = 0.;
-		grid[tIter].back() = 0.;
-		time += tau;
-	}
+	//	//grid[tIter].back() = u0;
+	//	grid[tIter].front() = 0.;
+	//	grid[tIter].back() = 0.;
+	//	time += tau;
+	//}
 
 	double taucroh = tau / (c * ro * h);
 	double crohtau = c * ro * h / tau;
 	double aPres, aNext;
 
+	double time = 0.;
 	if (sig == 0.)
 	{
 		//В предположении, что сигма 0 (Явная схема)
 		for (int tIter = 0; tIter < Nt - 1; tIter++)
 		{
+			//Заполнение краевыми значениями на новом временном слое
+			//Разве нужно это делать на каждой итерации? (для примера не зависящего от тау - нет)
+			time += tau;
+			grid[1].front() = 0.;
+			grid[1].back() = 0.;
+
 			x = h;
 			for (int pIter = 1; pIter < Nx - 1; pIter++)
 			{
-				aPres = 0.5 * (K(grid[tIter][pIter], x) + K(grid[tIter][pIter - 1], x - h));
-				aNext = 0.5 * (K(grid[tIter][pIter + 1], x + h) + K(grid[tIter][pIter], x));
+				aPres = 0.5 * (K(grid[0][pIter], x) + K(grid[0][pIter - 1], x - h));
+				aNext = 0.5 * (K(grid[0][pIter + 1], x + h) + K(grid[0][pIter], x));
 
 				//Явная разностная схема
-				grid[tIter + 1][pIter] = grid[tIter][pIter] + taucroh * (aNext * (grid[tIter][pIter + 1] - grid[tIter][pIter]) / h - aPres * (grid[tIter][pIter] - grid[tIter][pIter - 1]) / h);
+				grid[1][pIter] = grid[0][pIter] + taucroh * (aNext * (grid[0][pIter + 1] - grid[0][pIter]) / h - aPres * (grid[0][pIter] - grid[0][pIter - 1]) / h);
 
 				x += h;
 			}
+			grid[0] = grid[1];
 		}
 	}
 	else
@@ -85,65 +92,74 @@ void Equation::Scheme(double sig)
 
 		for (size_t tIter = 0; tIter < Nt - 1; tIter++)
 		{
+			//Заполнение краевыми значениями на новом временном слое
+			time += tau;
+			grid[1].front() = 0.;
+			grid[1].back() = 0.;
+
 			x = h;
-			//Первая итерация
-			aPres = 0.5 * (K(grid[tIter][1], x) + K(grid[tIter][0], x - h));
-			aNext = 0.5 * (K(grid[tIter][2], x + h) + K(grid[tIter][1], x));
-			A = sigh * aPres;
-			B = sigh * aNext;
-			C = sigh * aPres + sigh * aNext + crohtau;
-			F = crohtau * grid[tIter][1] + (1 - sig) * (aNext * (grid[tIter][2] - grid[tIter][1]) / h - aPres * (grid[tIter][1] - grid[tIter][0]) / h);
-
-			alpha.push_back(B / C);
-			betta.push_back((F + A * grid[tIter + 1][0]) / C);
-
-			for (size_t pIter = 2; pIter < Nx - 2; pIter++)
-			{
-				x += h;
-				aPres = 0.5 * (K(grid[tIter][pIter], x) + K(grid[tIter][pIter - 1], x - h));
-				aNext = 0.5 * (K(grid[tIter][pIter + 1], x + h) + K(grid[tIter][pIter], x));
-
+			/*size_t Iter = 0;
+			for (Iter; Iter < 5; Iter++)
+			{*/
+				//Первая итерация
+				aPres = 0.5 * (K(grid[0][1], x) + K(grid[0][0], x - h));
+				aNext = 0.5 * (K(grid[0][2], x + h) + K(grid[0][1], x));
 				A = sigh * aPres;
 				B = sigh * aNext;
 				C = sigh * aPres + sigh * aNext + crohtau;
-				F = crohtau * grid[tIter][pIter] + (1 - sig) * (aNext * (grid[tIter][pIter + 1] - grid[tIter][pIter]) / h - aPres * (grid[tIter][pIter] - grid[tIter][pIter - 1]) / h);
+				F = crohtau * grid[0][1] + (1 - sig) * (aNext * (grid[0][2] - grid[0][1]) / h - aPres * (grid[0][1] - grid[0][0]) / h);
+
+				alpha.push_back(B / C);
+				betta.push_back((F + A * grid[1][0]) / C);
+
+				for (size_t pIter = 2; pIter < Nx - 2; pIter++)
+				{
+					x += h;
+					aPres = 0.5 * (K(grid[0][pIter], x) + K(grid[0][pIter - 1], x - h));
+					aNext = 0.5 * (K(grid[0][pIter + 1], x + h) + K(grid[0][pIter], x));
+
+					A = sigh * aPres;
+					B = sigh * aNext;
+					C = sigh * aPres + sigh * aNext + crohtau;
+					F = crohtau * grid[0][pIter] + (1 - sig) * (aNext * (grid[0][pIter + 1] - grid[0][pIter]) / h - aPres * (grid[0][pIter] - grid[0][pIter - 1]) / h);
+
+					temp = C - A * alpha.back();
+
+					alpha.push_back(B / temp);
+					betta.push_back((A * betta.back() + F) / temp);	//TODO: знак перед F
+
+				}
+				x += h;
+				//Последняя итерация
+				aPres = 0.5 * (K(grid[0][Nx - 2], x) + K(grid[0][Nx - 3], x - h));
+				aNext = 0.5 * (K(grid[0][Nx - 1], x + h) + K(grid[0][Nx - 2], x));
+				A = sigh * aPres;
+				B = sigh * aNext;
+				C = sigh * aPres + sigh * aNext + crohtau;
+				F = crohtau * grid[0][Nx - 2] + (1 - sig) * (aNext * (grid[0][Nx - 1] - grid[0][Nx - 2]) / h - aPres * (grid[0][Nx - 2] - grid[0][Nx - 3]) / h);
 
 				temp = C - A * alpha.back();
 
-				alpha.push_back(B / temp);
-				betta.push_back((A * betta.back() + F) / temp);	//TODO: знак перед F
+				grid[1][Nx - 2] = (A * betta.back() + F + B * grid[1][Nx - 1]) / temp;
 
-			}
-			x += h;
-			//Последняя итерация
-			aPres = 0.5 * (K(grid[tIter][Nx - 2], x) + K(grid[tIter][Nx - 3], x - h));
-			aNext = 0.5 * (K(grid[tIter][Nx - 1], x + h) + K(grid[tIter][Nx - 2], x));
-			A = sigh * aPres;
-			B = sigh * aNext;
-			C = sigh * aPres + sigh * aNext + crohtau;
-			F = crohtau * grid[tIter][Nx - 2] + (1 - sig) * (aNext * (grid[tIter][Nx - 1] - grid[tIter][Nx - 2]) / h - aPres * (grid[tIter][Nx - 2] - grid[tIter][Nx - 3]) / h);
+				//Обратный ход
+				for (size_t pIter = Nx - 3; pIter > 0; pIter--)
+				{
+					grid[1][pIter] = alpha[pIter - 1] * grid[1][pIter + 1] + betta[pIter - 1];
+				}
 
-			temp = C - A * alpha.back();
+				alpha.clear();
+				betta.clear();
 
-			grid[tIter + 1][Nx - 2] = (A * betta.back() + F + B * grid[tIter + 1][Nx - 1]) / temp;
-
-			//Обратный ход
-			for (size_t pIter = Nx - 3; pIter > 0; pIter--)
-			{
-				grid[tIter + 1][pIter] = alpha[pIter - 1] * grid[tIter + 1][pIter + 1] + betta[pIter - 1];
-			}
-
-			alpha.clear();
-			betta.clear();
+				grid[0] = grid[1];
+			//}
 		}
 	}
 
 	//Вывод в файл
-	for (const auto gt : grid)
+	for (const auto gx : grid[0])
 	{
-		for (const auto gx : gt)
-			out << gx << " ";
-		out << "\n";
+		out << gx << " ";
 	}
 }
 
@@ -235,7 +251,7 @@ void Equation::SchemeEx3(double sig, double u0)
 				temp = C - A * alpha.back();
 
 				alpha.push_back(B / temp);
-				betta.push_back((A * betta.back() + F) / temp);	
+				betta.push_back((A * betta.back() + F) / temp);
 
 			}
 			x += h;
--- LR_2/LR_2.cpp
+++ LR_2/LR_2.cpp
@@ -7,12 +7,12 @@ int main()
 	double cappa = .5, delta = 2., cc = 5;
 	double t_u0 = pow(delta * cc * cc / cappa, 1 / delta);
 
-	Equation first(ThermCondExZero, std::string("example0ExH2"));
+	Equation first(ThermCondExZero, std::string("example0Im"));
 	//Equation first(ThermCondX, std::string("example3Ex"));
 	//Equation first(ThermCondU);
 
 	//Параметр сигма(от 0 до 1) разностной схемы 
-	double sigma = .0;
+	double sigma = 1.;
 
 	first.Scheme(sigma);
 	//first.SchemeEx3(sigma, t_u0);
